791199052e7d9fbf09fed709a14c0a50ebe59a9b
xiaomu <1105688871@qq.com>
2022-10-18 17:09:36 +0800

    表单标签

--- src/views/login/LoginIndex.vue
+++ src/views/login/LoginIndex.vue
@@ -6,13 +6,45 @@
       <!-- 标题的盒子 -->
       <div class="title-box"></div>
       <!-- 注册的表单区域 -->
+      <el-form ref="form" :model="form">
+        <el-form-item>
+          <el-input placeholder="请输入用户名" v-model="form.username"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input placeholder="请输入密码" v-model="form.password"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input placeholder="请再次确认密码" v-model="form.repassword"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button class="btn-reg" type="primary" @click="registerFn">注册</el-button>
+          <el-link type="info">去登录</el-link>
+        </el-form-item>
+      </el-form>
     </div>
   </div>
 </template>
 
 <script>
+// 前端绑定数据对象属性名，可以直接给要调用的功能接口参数名一致
+// 好处：我可以直接把前端对象（带着同名的属性和前端的值）发给后台
 export default {
-  name: 'my-register'
+  name: 'my-register',
+  data () {
+    return {
+      form: {
+        username: '', // 用户名
+        password: '', // 密码
+        repassword: '' // 确认密码
+      }
+    }
+  },
+  methods: {
+    // 注册点击事件
+    registerFn () {
+
+    }
+  }
 }
 </script>
 
