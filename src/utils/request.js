b5f355d98a7513042bffacb1547b429293e8c88c
xiaomu <1105688871@qq.com>
2022-10-19 18:13:15 +0800

    axios响应拦截器，被动退出

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,8 @@
 // 基于axios封装，网络请求函数
 import axios from 'axios'
 import store from '@/store'
+import router from '@/router'
+import { Message } from 'element-ui'
 
 // axios.create()创建一个带配置项的自定义axios函数
 // myAxios请求的时候，地址baseURL+URL，然后去请求后台
@@ -35,6 +37,26 @@ myAxios.interceptors.request.use(function (config) {
   // return new Promise((resolve, reject) => {
   //   reject(error)
   // })
+})
+
+// 定义响应拦截器
+myAxios.interceptors.response.use(function (response) {
+  // 响应状态码为 2xx 时触发成功的回调，形参中的 response 是“成功的结果”
+  // return到axios原地Promise对象，作为成功的结果
+  return response
+}, function (error) {
+  // 响应状态码不是 2xx 时触发失败的回调，形参中的 error 是“失败的结果”
+  // return到axios原地Promise对象，作为失败拒绝的状态（如果那边用try+catch或者用catch函数捕获，可以捕获到我们传递过去的这个error变量的值）
+  if (error.response.status === 401) {
+    // 本次响应是token过期了
+    // 清除vuex里的一切然后切换回登录页面（被动退出登录状态）
+    store.commit('updateToken', '')
+    store.commit('updateUserInfo', {})
+    // 跳转到登录页面
+    router.push('/login')
+    Message.error('用户身份已过期！')
+  }
+  return Promise.reject(error)
 })
 
 // 导出axios自定义函数
