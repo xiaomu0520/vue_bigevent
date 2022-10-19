290d8c378cb33c965737c6d16ae2b8bbc4d09eb8
xiaomu <1105688871@qq.com>
2022-10-19 17:44:10 +0800

    未登录不能去正常页面显示 权限

--- src/router/index.js
+++ src/router/index.js
@@ -27,15 +27,32 @@ const routes = [
 const router = new VueRouter({
   routes
 })
+
+const whiteList = ['/login', '/reg'] // 白名单（无需登录就可以访问的路由地址）
+
 // 全局前置路由守卫
+// 知识点1：浏览器第一次打开项目页面，会触发一次全局前置路由守卫
+// 知识点2：判断：有token就证明登录了，没有就是没登录
+// 知识点3：next()如果强制切换路由地址，会再次走路由守卫再次去匹配路由表
 router.beforeEach((to, from, next) => {
   const token = store.state.token
-  if (token && !store.state.userInfo.username) {
-    // 现在需要有本地的token值，才能去请求
-    store.dispatch('getUserInfoActions')
+  if (token) {
+    // 登录了
+    if (token && !store.state.userInfo.username) {
+      // 现在需要有本地的token值，才能去请求
+      store.dispatch('getUserInfoActions')
+    }
+    next()
+  } else {
+    // 数组.includes(值)，作用：判断值是否在数组里面出现过
+    if (whiteList.includes(to.path)) {
+      // 未登录，访问可以访问的路由地址，则放行(路由前置全局守卫不再触发了，而是匹配路由表，让组件挂载)
+      next()
+    } else {
+      // 未登录，就next()跳转到登录页面
+      next('/login')
+    }
   }
-
-  next()
 })
 
 export default router
