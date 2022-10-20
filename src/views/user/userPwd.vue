c388c56e0cd03adbaf78738a19e1dda26f0a4a23
xiaomu <1105688871@qq.com>
2022-10-20 15:26:58 +0800

    重置密码组件和路由

---
+++ src/views/user/userPwd.vue
@@ -0,0 +1,60 @@
+<template>
+  <el-card class="box-card">
+    <div slot="header" class="clearfix">
+      <span>重置密码</span>
+    </div>
+    <!-- 表单 -->
+    <el-form :model="pwdForm" :rules="pwdFormRules" ref="pwdFormRef" label-width="100px">
+      <el-form-item label="原密码" prop="old_pwd">
+        <el-input v-model="pwdForm.old_pwd" type="password"></el-input>
+      </el-form-item>
+      <el-form-item label="新密码" prop="new_pwd">
+        <el-input v-model="pwdForm.new_pwd" type="password"></el-input>
+      </el-form-item>
+      <el-form-item label="确认新密码" prop="re_pwd">
+        <el-input v-model="pwdForm.re_pwd" type="password"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary">修改密码</el-button>
+        <el-button>重置</el-button>
+      </el-form-item>
+    </el-form>
+  </el-card>
+</template>
+
+<script>
+export default {
+  name: 'UserPwd',
+  data () {
+    return {
+      // 表单的数据对象
+      pwdForm: {
+        old_pwd: '',
+        new_pwd: '',
+        re_pwd: ''
+      },
+      // 表单的验证规则对象
+      pwdFormRules: {
+        old_pwd: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { pattern: /^\S{6,15}$/, message: '密码长度必须是6-15位的非空字符串', trigger: 'blur' }
+        ],
+        new_pwd: [
+          { required: true, message: '请输入新密码', trigger: 'blur' },
+          { pattern: /^\S{6,15}$/, message: '密码长度必须是6-15位的非空字符串', trigger: 'blur' }
+        ],
+        re_pwd: [
+          { required: true, message: '请再次确认新密码', trigger: 'blur' },
+          { pattern: /^\S{6,15}$/, message: '密码长度必须是6-15位的非空字符串', trigger: 'blur' }
+        ]
+      }
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.el-form {
+  width: 500px;
+}
+</style>
